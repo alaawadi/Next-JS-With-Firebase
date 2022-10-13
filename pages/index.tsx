0a8eeb248dcc369e35ca3697016faf970584d705
alaawadi <alaawadi98@gmail.com>
2022-10-13 17:17:03 +0300

    First Commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useAuth } from "../firebase/hook/AuthContext";
+import { useRouter } from "next/router";
+import Header from "../components/header";
+
+const Home: NextPage = () => {
+  const auth = useAuth();
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <div>{auth.user?.displayName}</div>
+      <div>{auth.user?.email}</div>
+      <div>{auth.user?.uid}</div>
+      {auth.user ? <>you are loged in</> : <>you are loged out</>}
+    </>
+  );
+};
+
+export default Home;
